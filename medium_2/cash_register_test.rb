65a17ff7ae4feb08db2efeda0b4a57120e500cb9
Raul Romero <raulromerocano@aggienetwork.com>
2021-01-04 14:12:06 +0000

    Implemented test for #change

--- medium_2/cash_register_test.rb
+++ medium_2/cash_register_test.rb
@@ -17,11 +17,22 @@ class CashRegisterTest < Minitest::Test
     initial_amount_register = register.total_money
     
     register.accept_money(transaction) 
-    assert_equal(initial_amount_register, register.total_money) # Value stays the same because 
-                                                                # payment has not been made yet
+    assert_equal(initial_amount_register, register.total_money)   # Value stays the same because 
+                                                                  # payment has not been made yet
     
-    transaction.amount_paid = 50                                # Payment of $50 was made
-    register.accept_money(transaction)                          # Register total money should go up by $50
+    transaction.amount_paid = 50                                  # Payment of $50 was made
+    register.accept_money(transaction)                            # Register total money should go up by $50
     assert_equal(initial_amount_register + 50, register.total_money) 
+  end
+  
+  def test_change
+    # Testing change with no payment given
+    change_with_no_payment = register.change(transaction)         # Should be a negative value
+    assert_equal change_with_no_payment, transaction.item_cost * -1
+    
+    # Testing change by paying more than item cost
+    transaction.amount_paid = 100
+    change_with_payment = register.change(transaction)
+    assert_equal change_with_payment, 50
   end
 end
